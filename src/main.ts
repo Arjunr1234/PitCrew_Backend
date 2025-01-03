843d9a6462b059ee8358b2b40eaea3042ac0d375
Arjunr1234 <84847522+Arjunr1234@users.noreply.github.com>
2025-01-03 18:26:19 +0530

    pushing details

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import connectDB from "./framework/config/db";
 connectDB()
 
 server.listen(3000, () => {
-    console.log('arjun started on port 3000');
+    console.log('Server started on port 3000');
     
 });
